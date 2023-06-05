121694733335659d4a2bcdbb4e4cbac41c3553eb
Anthony Christodoulou <christodoulou75@gmail.com>
2023-06-05 17:19:50 +0200

    Routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :gardens do
+    resources :garden_plants, only: [:create]
+    resources :tasks, only: [:index]
+  end
+  resources :wishlists, only: [:show] do
+    resources :wishlist_plants, only: [:create]
+  end
+  resources :plants, only: [] do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :wishlist_plants, only: [:destroy]
+  resources :garden_plants, only: [:show, :destroy, :update]
+  resources :tasks, only: [:update]
 end
