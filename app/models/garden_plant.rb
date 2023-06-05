dac486bd48292e65c4603e9f90921ffcb140c9d4
Anthony Christodoulou <christodoulou75@gmail.com>
2023-06-05 17:50:11 +0200

    added validates in garden plants

--- app/models/garden_plant.rb
+++ app/models/garden_plant.rb
@@ -2,4 +2,6 @@ class GardenPlant < ApplicationRecord
   belongs_to :plant
   belongs_to :garden
   has_many :tasks
+  validates :nickname, :pot_color, presence: true
+  # validates :pot_color, inclusion: {in: %w()}
 end
