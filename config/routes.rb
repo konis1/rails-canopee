c2e0909faca600017fd8e693503376d5b9d11228
nicolas bourdin <nicolas_bourdin@hotmail.com>
2023-06-06 11:13:25 +0200

    resources mises à jour

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,11 @@ Rails.application.routes.draw do
     resources :garden_plants, only: [:create]
     resources :tasks, only: [:index]
   end
-  resources :wishlists, only: [:show] do
-    resources :wishlist_plants, only: [:create]
-  end
+
   resources :plants, only: [] do
     resources :reviews, only: [:new, :create]
   end
-  resources :wishlist_plants, only: [:destroy]
+  
   resources :garden_plants, only: [:show, :destroy, :update]
   resources :tasks, only: [:update]
 end
