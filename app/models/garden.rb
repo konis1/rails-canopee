b261eb86078aa8823d778b168e66951c8d583aca
nicolas bourdin <nicolas_bourdin@hotmail.com>
2023-06-06 15:27:02 +0200

    modif erreur frappe

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -3,7 +3,7 @@ class Garden < ApplicationRecord
   has_many :plants, through: :garden_plants
   has_many :garden_plants, dependent: :destroy
   validates :light, :size, :location, presence: true
-  validated :name, uniqueness: true
+  validates :name, uniqueness: true
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 end
