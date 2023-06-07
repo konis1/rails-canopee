8aa36ef59d65429f47e7442904a13b14221cf617
Anthony Christodoulou <christodoulou75@gmail.com>
2023-06-07 14:58:35 +0200

    changed error migration

--- db/migrate/20230607094422_add_climate_to_plants.rb
+++ db/migrate/20230607094422_add_climate_to_plants.rb
@@ -1,7 +1,5 @@
 class AddClimateToPlants < ActiveRecord::Migration[7.0]
   def change
-    def change
-      add_column :plants, :climate, :string, array: true
-    end
+    add_column :plants, :climate, :string, array: true
   end
 end
