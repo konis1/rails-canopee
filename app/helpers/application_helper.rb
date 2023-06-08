4982b297373ebf720568ca839a2775f1acfbfc2e
nicolas bourdin <nicolas_bourdin@hotmail.com>
2023-06-08 16:14:58 +0200

    index encore plus mieux

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def page_background_color_class
+    if current_page?(gardens_path)
+      "bg-green"
+    elsif current_page?(root_path)
+      "bg-red"
+    end
+  end
 end
