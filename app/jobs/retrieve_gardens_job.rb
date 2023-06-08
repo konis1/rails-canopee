97b226f9fda3d335304e990be1c092dfd32e952f
Julien Pons <jp93rom@orange.fr>
2023-06-08 14:28:55 +0200

    Generate the plant iteration job, the garden retrieval job refers to it.

--- app/jobs/retrieve_gardens_job.rb
+++ app/jobs/retrieve_gardens_job.rb
@@ -6,15 +6,7 @@ class RetrieveGardensJob < ApplicationJob
     gardens.each do |garden|
       weather_service = WeatherService.new(garden.latitude, garden.longitude, garden.location)
       weather_data = weather_service.retrieve_16_days_forecast
-      min_temp_array = weather_data[0]
-      max_temp_array = weather_data[1]
-      rain_array = weather_data[2]
-      past_min_temp_array = min_temp_array.first(7)
-      past_max_temp_array = max_temp_array.first(7)
-      past_rain_array = rain_array.first(7)
-      forecast_min_temp_array = min_temp_array.drop(7)
-      forecast_max_temp_array = max_temp_array.drop(7)
-      forecast_rain_array = rain_array.drop(7)
+      RetrieveGardenPlantsJob.perform_later(garden, weather_data)
     end
   end
 end
