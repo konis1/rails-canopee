20d0a2510582b868e06886e9f34fcf9685d1e135
mj <malika.housni@gmail.com>
2023-06-09 12:45:27 +0200

    navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,5 @@ class User < ApplicationRecord
          has_many :gardens, dependent: :destroy
          has_many :reviews, dependent: :destroy
 
-         after_commit :async_update, on: [:create, :update]
 
-  private
-
-  def async_update
-    UpdateUserJob.perform_later(self)
-  end
 end
