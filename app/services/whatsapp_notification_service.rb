771e598812a0e540e22e53332bdd6ba9ada6a2ac
nicolas bourdin <nicolas_bourdin@hotmail.com>
2023-09-15 18:39:38 +0200

    tentative push scalingo

--- app/services/whatsapp_notification_service.rb
+++ app/services/whatsapp_notification_service.rb
@@ -1,25 +1,25 @@
-# class WhatsappNotificationService
-#   require 'rubygems'
-#   require 'twilio-ruby'
+class WhatsappNotificationService
+  require 'rubygems'
+  require 'twilio-ruby'
 
-#   def initialize(phone_number, message)
-#     @phone_number = phone_number
-#     @message = message
-#   end
+  def initialize(phone_number, message)
+    @phone_number = phone_number
+    @message = message
+  end
 
-#   def send
-#     account_sid = ENV['account_sid']
-#     auth_token = ENV['auth_token']
+  def send
+    account_sid = ENV['account_sid']
+    auth_token = ENV['auth_token']
 
-#   begin
-#     @client = Twilio::REST::Client.new(account_sid, auth_token)
-#     message = @client.messages.create(
-#       body: @message,
-#       from: 'whatsapp:+14155238886',
-#       to: "whatsapp:#{@phone_number}"
-#     )
-#   rescue Twilio::REST::RestError => e
-#     puts "Error sending message: #{e.message}"
-#     end
-#   end
-# end
+  begin
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
+    message = @client.messages.create(
+      body: @message,
+      from: 'whatsapp:+14155238886',
+      to: "whatsapp:#{@phone_number}"
+    )
+  rescue Twilio::REST::RestError => e
+    puts "Error sending message: #{e.message}"
+    end
+  end
+end
