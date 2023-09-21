65a4bd7ec438940a705ba0328b5cfdeed5c3fecd
nicolas bourdin <nicolas_bourdin@hotmail.com>
2023-09-21 15:22:45 +0200

    impossible de faire fonctionner le mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "bonnefeuilleparis@gmail.com"
-  layout "welcome_email"
+  # default from: "bonnefeuilleparis@gmail.com"
+  # layout "welcome_email"
 
   def welcome_email(user)
     @user = user
     mail(to: @user.email, subject: 'Bienvenue sur notre site! Merci pour votre commande')
   end
+
 end
