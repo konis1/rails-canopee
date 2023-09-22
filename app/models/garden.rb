93d58ff6571dc6500c6711a4b106ca7ec804e426
nicolas bourdin <nicolas_bourdin@hotmail.com>
2023-09-22 17:42:47 +0200

    selecting logic changed

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -10,41 +10,51 @@ class Garden < ApplicationRecord
   after_create_commit :associate_plants
 
 
-  # def associate_plants
-
-  #   # garden = self
-  #   # recuperer tyoutes les instances de plantes correspondant a ce jardin
-  #   plants = Plant.where("? = ANY(climate)", "Climat #{self.climate}").where(
-  #     light_need: self.light,
-  #     care_frequency: self.care_willing
-  #     # TODO add other criterias
-  #   )
-  #   # et créer les garden_plants correspondantes
-  #   self.plants << plants
-  #   save
-  # end
   def associate_plants
-    # Récupérer toutes les instances de plantes correspondant à ce jardin
-    plants = Plant.where("? = ANY(climate)", "Climat #{self.climate}")
+    # Récupérer toutes les instances de plantes correspondant à ce jardin en fonction des critères
 
-    # Filtrer les plantes en fonction du light_need
-    if [0].include?(self.light)
-      plants = plants.(Plant.where(light_need: [0]))
-    elsif [2].include?(self.light)
-      plants = plants.or(Plant.where(light_need: [2]))
+
+    def careness
+      # Vous semblez vouloir utiliser self.care_willing comme condition, mais cela doit être écrit correctement
+      if self.care_willing == 0
+        return Plant.where(care_frequency: 0)
+      elsif self.care_willing == 1
+        return (Plant.where(care_frequency: 0) + Plant.where(care_frequency: 1)).uniq
+      else
+        return (Plant.where(care_frequency: 0) + Plant.where(care_frequency: 1) + Plant.where(care_frequency: 2)).uniq
+      end
     end
 
-    # Filtrer les plantes en fonction du care_willing
-    if [2].include?(self.care_willing)
-      plants = plants.or(Plant.where(care_frequency: [2]))
-    elsif [1].include?(self.care_willing)
-      plants = plants.or(Plant.where(care_frequency: [1]))
-    end
+    # Sélectionner les plantes en fonction des critères et de la volonté de soins
+    plants = Plant.where("? = ANY(climate)", "Climat #{self.climate}").where(light_need: self.light).merge(careness)
 
-    # Créer les garden_plants correspondantes
+    # Ajouter les plantes au jardin
     self.plants << plants
     save
   end
+
+  # def associate_plants
+  #   # Récupérer toutes les instances de plantes correspondant à ce jardin
+  #   plants = Plant.where("? = ANY(climate)", "Climat #{self.climate}")
+
+  #   # Filtrer les plantes en fonction du light_need
+  #   if [0].include?(self.light)
+  #     plants = plants.(Plant.where(light_need: [0]))
+  #   elsif [2].include?(self.light)
+  #     plants = plants.or(Plant.where(light_need: [2]))
+  #   end
+
+  #   # Filtrer les plantes en fonction du care_willing
+  #   if [2].include?(self.care_willing)
+  #     plants = plants.or(Plant.where(care_frequency: [2]))
+  #   elsif [1].include?(self.care_willing)
+  #     plants = plants.or(Plant.where(care_frequency: [1]))
+  #   end
+
+  #   # Créer les garden_plants correspondantes
+  #   self.plants << plants
+  #   save
+  # end
 
 
 
